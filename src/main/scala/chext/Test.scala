c1d0621408d6c8198b8d21f6e68adf407e8d2e09
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2024-02-11 20:35:37 +0300

    Elastic tests.

--- src/main/scala/chext/Test.scala
+++ src/main/scala/chext/Test.scala
@@ -1,6 +1,30 @@
-package chext
+package chext.test
 
-object MyApp extends App {
-  import axi4.Ops._
-  val interface = axi4.Interface(axi4.Config())
+import chisel3._
+import chiseltest._
+
+import org.scalatest.freespec.AnyFreeSpec
+import scala.collection.mutable.ArrayBuffer
+
+abstract class TesterSpec extends AnyFreeSpec with ChiselScalatestTester {
+  private val annotations = ArrayBuffer.empty[firrtl2.annotations.Annotation]
+  protected val rand = new scala.util.Random
+
+  protected final def clearAnnotations() = annotations.clear()
+  protected final def addAnnotation(anno: firrtl2.annotations.Annotation) =
+    annotations.addOne(anno)
+  protected final def enableVcd() =
+    addAnnotation(WriteVcdAnnotation)
+  protected final def useVerilator() =
+    addAnnotation(VerilatorBackendAnnotation)
+
+  override def test[T <: Module](dutGen: => T): TestBuilder[T] =
+    super.test(dutGen).withAnnotations(annotations.toSeq)
+
+  def stepRandom(maxExclusive: Int): Unit = stepRandom(1, maxExclusive)
+
+  def stepRandom(minInclusive: Int, maxExclusive: Int): Unit = {
+    assert(minInclusive > 0, "The number of steps should be at least 1.")
+    step(rand.between(minInclusive, maxExclusive))
+  }
 }
