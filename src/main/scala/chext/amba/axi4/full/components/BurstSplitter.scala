1bc9ea0ce5d5706a7bc74298255c0b81baad80ba
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2024-09-23 09:25:44 +0200

    Burst splitter write channel as well.

--- src/main/scala/chext/amba/axi4/full/components/BurstSplitter.scala
+++ src/main/scala/chext/amba/axi4/full/components/BurstSplitter.scala
@@ -12,10 +12,14 @@ import axi4.Ops._
 import axi4.full.components.addrgen
 
 case class BurstSplitterConfig(
-    val axiCfg: axi4.Config
+    val axiCfg: axi4.Config,
+    val arQueueCapacity: Int = 8,
+    val awQueueCapacity: Int = 8
 ) {
   assert(axiCfg.wId == 0, "axiCfg.wId must be zero!")
   assert(!axiCfg.lite, "axiCfg.lite must be false!")
+  assert(arQueueCapacity >= 2, "AR queue capacity must be >= 2!")
+  assert(awQueueCapacity >= 2, "AW queue capacity must be >= 2!")
 
   val wAddr = axiCfg.wAddr
   val wData = axiCfg.wData
@@ -37,8 +41,10 @@ class BurstSplitter(val cfg: BurstSplitterConfig) extends Module {
       )
 
     val lenQueue = Module(
-      new Queue(UInt(axiCfg.wLen.W), 16 /* TODO make reconfigurable */ )
+      new Queue(UInt(axiCfg.wLen.W), arQueueCapacity)
     )
+
+    val arReplicated = Wire(chiselTypeOf(s_axi.ar))
 
     def implAR(): Unit = prefix("ar") {
       new elastic.Fork(s_axi.ar) {
@@ -52,43 +58,41 @@ class BurstSplitter(val cfg: BurstSplitterConfig) extends Module {
             }
           }
 
-          val wire0 = Wire(chiselTypeOf(s_axi.ar))
-
-          new elastic.Replicate(fork(), wire0) {
+          new elastic.Replicate(fork(), arReplicated) {
             protected def onReplicate: Unit = {
               len := in.len +& 1.U
               out := in
             }
           }
 
-          new elastic.Join(m_axi.ar) {
-            protected def onJoin: Unit = {
-              val pkt0 = join(wire0)
-              val pkt1 = join(addressStrobeGenerator.sink)
-
-              out.id := pkt0.id
-              out.addr := pkt1.addr
-              out.len := 0.U
-              out.size := pkt1.size
-              out.burst := 1.U
-              out.lock := pkt0.lock
-              out.cache := pkt0.cache
-              out.prot := pkt0.prot
-              out.qos := pkt0.qos
-              out.region := pkt0.region
-              out.user := pkt0.user
-            }
-          }
-
           fork(in.len) :=> lenQueue.io.enq
+        }
+      }
+
+      new elastic.Join(m_axi.ar) {
+        protected def onJoin: Unit = {
+          val pkt0 = join(arReplicated)
+          val pkt1 = join(addressStrobeGenerator.sink)
+
+          out.id := pkt0.id
+          out.addr := pkt1.addr
+          out.len := 0.U
+          out.size := pkt1.size
+          out.burst := 1.U
+          out.lock := pkt0.lock
+          out.cache := pkt0.cache
+          out.prot := pkt0.prot
+          out.qos := pkt0.qos
+          out.region := pkt0.region
+          out.user := pkt0.user
         }
       }
     }
 
     def implR(): Unit = prefix("r") {
-      val wire0 = Wire(DecoupledIO(Bool()))
+      val lastReplicated = Wire(DecoupledIO(Bool()))
 
-      new elastic.Replicate(lenQueue.io.deq, wire0) {
+      new elastic.Replicate(lenQueue.io.deq, lastReplicated) {
         protected def onReplicate: Unit = {
           len := in +& 1.U
           out := last
@@ -98,7 +102,7 @@ class BurstSplitter(val cfg: BurstSplitterConfig) extends Module {
       new elastic.Join(s_axi.r) {
         protected def onJoin: Unit = {
           val r = join(m_axi.r)
-          val last = join(wire0)
+          val last = join(lastReplicated)
 
           out := r
           out.last := last
@@ -111,10 +115,108 @@ class BurstSplitter(val cfg: BurstSplitterConfig) extends Module {
   }
 
   private def implWrite(): Unit = prefix("write") {
-    // TODO impl
-    s_axi.aw :=> m_axi.aw
-    s_axi.w :=> m_axi.w
-    m_axi.b :=> s_axi.b
+    val addressStrobeGenerator =
+      Module(
+        new addrgen.AddressStrobeGenerator(wAddr, wData)
+      )
+
+    val lenQueue = Module(
+      new Queue(UInt(axiCfg.wLen.W), awQueueCapacity)
+    )
+
+    val awReplicated = Wire(chiselTypeOf(s_axi.aw))
+
+    def implAW(): Unit = {
+      new elastic.Fork(s_axi.aw) {
+        protected def onFork: Unit = {
+          new elastic.Transform(fork(), addressStrobeGenerator.source) {
+            protected def onTransform: Unit = {
+              out.addr := in.addr
+              out.len := in.len
+              out.size := in.size
+              out.burst := in.burst
+            }
+          }
+
+          new elastic.Replicate(fork(), awReplicated) {
+            protected def onReplicate: Unit = {
+              len := in.len +& 1.U
+              out := in
+            }
+          }
+
+          fork(in.len) :=> lenQueue.io.enq
+        }
+      }
+
+      new elastic.Join(m_axi.aw) {
+        protected def onJoin: Unit = {
+          val pkt0 = join(awReplicated)
+          val pkt1 = join(addressStrobeGenerator.sink)
+
+          out.id := pkt0.id
+          out.addr := pkt1.addr
+          out.len := 0.U
+          out.size := pkt1.size
+          out.burst := 1.U
+          out.lock := pkt0.lock
+          out.cache := pkt0.cache
+          out.prot := pkt0.prot
+          out.qos := pkt0.qos
+          out.region := pkt0.region
+          out.user := pkt0.user
+        }
+      }
+    }
+
+    def implW(): Unit = {
+      new elastic.Transform(s_axi.w, m_axi.w) {
+        protected def onTransform: Unit = {
+          out := in
+          out.last := true.B
+        }
+      }
+    }
+
+    def implB(): Unit = {
+      val lastReplicated = Wire(DecoupledIO(Bool()))
+      new elastic.Replicate(lenQueue.io.deq, lastReplicated) {
+        protected def onReplicate: Unit = {
+          len := in +& 1.U
+          out := last
+        }
+      }
+
+      val joined = elastic.Zip(m_axi.b, lastReplicated)
+
+      new elastic.Arrival(joined, s_axi.b) {
+        protected def onArrival: Unit = {
+          val respReg = RegInit(0.U(2.W))
+
+          out := in._1
+
+          // we reduce on the largest value of response
+          when(in._1.resp > respReg) {
+            respReg := in._1.resp
+            out.resp := in._1.resp
+          }.otherwise {
+            out.resp := respReg
+          }
+
+          when(in._2 /* last */ ) {
+            accept()
+
+            respReg := 0.U
+          }.otherwise {
+            drop()
+          }
+        }
+      }
+    }
+
+    implAW()
+    implW()
+    implB()
   }
 
   if (axiCfg.read) implRead()
