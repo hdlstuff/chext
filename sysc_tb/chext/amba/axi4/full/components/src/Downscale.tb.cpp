254fa1695fb37bf4baf8cc3355cd5b38275d4b7e
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2024-10-16 19:33:32 +0200

    AXI transactions.

--- sysc_tb/chext/amba/axi4/full/components/src/Downscale.tb.cpp
+++ sysc_tb/chext/amba/axi4/full/components/src/Downscale.tb.cpp
@@ -20,7 +20,7 @@ struct bv_from_impl;
 template<typename T>
 struct bv_from_impl<T, std::enable_if_t<std::is_integral_v<T>>> {
     static auto bv_from(T t, int width = (sizeof(T) * CHAR_BIT)) {
-        sc_bv_base result(width);
+        sc_bv_base result(width > 0 ? width : 32);
         result = t;
         return result;
     }
@@ -49,39 +49,58 @@ LOG2_IMPL(unsigned int, uint8_t, __builtin_clz);
 LOG2_IMPL(unsigned long, uint8_t, __builtin_clzl);
 LOG2_IMPL(unsigned long long, uint8_t, __builtin_clzll);
 
-sc_bv_base createStrobe() {
-}
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 using addr_t = uint64_t;
 
 struct Beat {
-    uint8_t index;
-
+    uint32_t index;
     uint64_t addr;
-    uint8_t size;
     bool last;
 
+    uint32_t lowerByteIndex;
+    uint32_t upperByteIndex;
+    uint32_t size;
     uint64_t strb;
-
-    uint8_t lowerByteIndex;
-    uint8_t upperByteIndex;
 
     JQR_DECL(
         Beat,
         JQR_MEMBER(index),
         JQR_MEMBER(addr, jqr::opts::dump_fmt { "{:#018x}" }),
-        JQR_MEMBER(size, jqr::opts::dump_fmt { "{:#04x}" }),
         JQR_MEMBER(last, jqr::opts::dump_fmt { "{:#03b}" }),
-        JQR_MEMBER(strb, jqr::opts::dump_fmt { "{:#010b}" }),
         JQR_MEMBER(lowerByteIndex),
-        JQR_MEMBER(upperByteIndex)
+        JQR_MEMBER(upperByteIndex),
+        JQR_MEMBER(size, jqr::opts::dump_fmt { "{:#04x}" }),
+        JQR_MEMBER(strb, jqr::opts::dump_fmt { "{:#010x}" })
     )
 
     JQR_TO_STRING
 };
 
-struct Transfer {
-    Transfer(axi4::full::Config const& cfg)
+void prepareData(sc_bv_base& out, uint8_t const* in, uint8_t lowerByteIndex, uint8_t upperByteIndex) {
+    uint8_t buffer[128] = { 0 };
+
+    for (unsigned i = lowerByteIndex; i <= upperByteIndex; ++i)
+        buffer[i] = *(in++);
+
+    for (unsigned wordIndex = 0; wordIndex < out.size(); ++wordIndex) {
+        static_assert(sizeof(sc_digit) == 4);
+
+        // clang-format off
+        out.set_word(
+            wordIndex,
+            (((sc_digit)buffer[wordIndex * 4]) << 0) |
+            (((sc_digit)buffer[wordIndex * 4 + 1]) << 8) |
+            (((sc_digit)buffer[wordIndex * 4 + 2]) << 16) |
+            (((sc_digit)buffer[wordIndex * 4 + 3]) << 24)
+        );
+        // clang-format on
+    }
+}
+
+struct Transaction {
+    Transaction(axi4::full::Config const& cfg)
         : cfg_ { cfg } {
         assert(cfg.wAddr <= 64);
         assert(cfg.wData <= 256);
@@ -100,8 +119,7 @@ struct Transfer {
 
             // the total size of the transfer should be aligned
             uint64_t transferSize = ((uint64_t)1) << size;
-            uint64_t transactionSize = ((uint64_t)1) << (log2numBeats + size);
-            assert((addr & (transferSize - 1)) == 0 && "for wrap burst, it must be aligned!");
+            assert((addr & (transferSize - 1)) == 0 && "for wrap burst, it must be aligned to the transfer size!");
         }
 
         firstAddr_ = addr;
@@ -119,21 +137,41 @@ struct Transfer {
 
         beat.~Beat();
 
-        if (beatIndex_ == 0) {
+        uint64_t busMask = (cfg_.wData / 8) - 1;
+
+        if (beatIndex_ == 0 || burst_ == 0) {
+            uint64_t addr = firstAddr_;
+
+            uint32_t lowerByteIndex = addr & busMask;
+            uint32_t upperByteIndex = (((1 + (addr >> size_)) << size_) - 1) & busMask;
+            uint32_t size = (upperByteIndex - lowerByteIndex) + 1;
+            uint64_t strb = ((((uint64_t)1) << size) - 1) << lowerByteIndex;
+
             new (&beat) Beat {
-                .index = (uint8_t)beatIndex_,
-                .addr = firstAddr_,
-                .size = size_,
+                .index = beatIndex_,
+                .addr = addr,
                 .last = (beatIndex_ == len_),
-                .strb = 0
+                .lowerByteIndex = lowerByteIndex,
+                .upperByteIndex = upperByteIndex,
+                .size = size,
+                .strb = strb
             };
         } else {
+            uint64_t addr = addr_ << size_;
+
+            uint32_t lowerByteIndex = addr & busMask;
+            uint32_t upperByteIndex = (((1 + (addr >> size_)) << size_) - 1) & busMask;
+            uint32_t size = (upperByteIndex - lowerByteIndex) + 1;
+            uint64_t strb = ((((uint64_t)1) << size) - 1) << lowerByteIndex;
+
             new (&beat) Beat {
-                .index = (uint8_t)beatIndex_,
-                .addr = addr_ << size_,
-                .size = size_,
+                .index = beatIndex_,
+                .addr = addr,
                 .last = (beatIndex_ == len_),
-                .strb = 0
+                .lowerByteIndex = lowerByteIndex,
+                .upperByteIndex = upperByteIndex,
+                .size = size,
+                .strb = strb
             };
         }
 
@@ -152,7 +190,7 @@ struct Transfer {
     }
 
 private:
-    axi4::full::Config cfg_;
+    axi4::full::Config const& cfg_;
 
     unsigned beatIndex_;
 
@@ -164,6 +202,73 @@ private:
     uint8_t burst_;
 };
 
+void simpleWrite(axi4::full::SlaveBase& target, uint64_t& addr, uint64_t& numBytes, unsigned char const* data, int size = -1) {
+    auto const& cfg = target.config();
+    unsigned maxSize = log2(cfg.wData / 8u);
+    size = (size >= 0 && size <= maxSize) ? size : maxSize;
+
+    uint64_t mask = (((uint64_t)1) << size) - 1;
+    uint64_t alignedAddr = addr & ~mask;
+    uint64_t alignedNumBytes = numBytes + addr - alignedAddr;
+
+    uint64_t numBeats = alignedNumBytes >> size;
+    if (alignedNumBytes & mask)
+        numBeats++;
+
+    uint8_t len = numBeats - 1;
+
+    // fmt::print("addr = {:08x}, numBytes = {}, size = {}, alignedAddr = {:08x}, numBeats = {}\n", addr, numBytes, size, alignedAddr, numBeats);
+
+    Transaction transaction(cfg);
+    transaction.reset(addr, len, size, 1);
+
+    sc_join j;
+
+    SC_SPAWN_TO(j) {
+        axi4::full::Packets::Address aw {
+            .id = bv_from(0, cfg.wId),
+            .addr = bv_from(addr, cfg.wAddr),
+            .size = (uint8_t)size,
+            .burst = 1,
+            .len = (uint8_t)len
+        };
+
+        target.sendAW(aw);
+        fmt::print("[t = {}] sent: {}\n", sc_time_stamp().to_string(), aw);
+    };
+
+    SC_SPAWN_TO(j) {
+        sc_bv_base bvData((int)cfg.wData);
+        sc_bv_base bvStrb((int)cfg.wStrb);
+
+        Beat b;
+
+        while (transaction.nextBeat(b)) {
+            prepareData(bvData, data, b.lowerByteIndex, b.upperByteIndex);
+
+            axi4::full::Packets::WriteData w {
+                .data = bvData,
+                .strb = bvStrb,
+                .last = b.last
+            };
+
+            target.sendW(w);
+            fmt::print("[t = {}] sent: {}\n", sc_time_stamp().to_string(), w);
+
+            addr += b.size;
+            data += b.size;
+            numBytes -= b.size;
+        }
+    };
+
+    SC_SPAWN_TO(j) {
+        auto b = target.receiveB();
+        fmt::print("[t = {}] received: {}\n", sc_time_stamp().to_string(), b);
+    };
+
+    j.wait();
+}
+/*
 void fillMemory(
     axi4::full::SlaveBase& target,
     uint64_t base,
@@ -177,11 +282,11 @@ void fillMemory(
     unsigned maxBeats = (1u << cfg.wLen);
 
     unsigned totalBeats = 1 + ((size - 1) / busBytes);
-    unsigned totalTransfers = 1 + ((totalBeats - 1) / maxBeats);
+    unsigned totalTransactions = 1 + ((totalBeats - 1) / maxBeats);
 
-    Transfer transfer(cfg);
+    Transaction transfer(cfg);
 
-    transfer.reset(base, 255, log2(busBytes), 1);
+    transfer.reset(base, 15, log2(busBytes), 1);
 
     sc_join j;
 
@@ -191,7 +296,7 @@ void fillMemory(
             .addr = bv_from(base, cfg.wAddr),
             .size = log2(busBytes),
             .burst = 1,
-            .len = 255
+            .len = 15
         };
 
         target.sendAW(aw);
@@ -221,6 +326,7 @@ void fillMemory(
 
     j.wait();
 }
+*/
 
 struct TwoInterfaceTest {
     TwoInterfaceTest(std::string const& name, axi4::full::SlaveBase& normal, axi4::full::SlaveBase& test)
@@ -229,9 +335,58 @@ struct TwoInterfaceTest {
         , test_ { test } {
     }
 
-    void runTest(uint64_t addr, uint64_t length) {
+    void runTest(uint64_t addr_, uint64_t length) {
         // step 1: fill in the memory range with data, in the form:
-        fillMemory(normal_, addr, length, nullptr);
+        // fillMemory(normal_, addr + 0x2, length, nullptr);
+
+        uint64_t addr, numBytes;
+
+        uint8_t data[256];
+
+        for (unsigned i = 0; i < 256; ++i)
+            data[i] = i;
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x00;
+        numBytes = 32;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x01;
+        numBytes = 32;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x02;
+        numBytes = 32;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x03;
+        numBytes = 32;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x04;
+        numBytes = 32;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
+
+        fmt::print("{} {}:{}\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+        addr = 0x02;
+        numBytes = 4;
+        simpleWrite(normal_, addr, numBytes, data);
+
+        wait(5, SC_NS);
     }
 
 private:
