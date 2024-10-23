61ac0863aa5eaf9a3b930547ec39233bbc3dbd72
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2024-10-24 00:01:47 +0200

    Refactor IdParallelize.tb.cpp for improved performance and readability.

--- sysc_tb/chext/amba/axi4/full/components/src/IdParallelize.tb.cpp
+++ sysc_tb/chext/amba/axi4/full/components/src/IdParallelize.tb.cpp
@@ -7,6 +7,9 @@
 #include <Util.hpp>
 #include <chext_test/chext_test.hpp>
 
+#include <chrono>
+#include <random>
+
 using namespace sc_core;
 using namespace sc_dt;
 
@@ -23,7 +26,9 @@ struct MyTestBench : virtual TestBenchBase {
         , dut { "dut" }
         , clock { "clock", 2.0, SC_NS }
         , reset { "reset" }
-        , arFifo { "arFifo" } {
+        , numThreads { 1u << dut.M_AXI.config().wId }
+        , arFifos { numThreads }
+        , rFifo {} {
 
         dut.clock(clock);
         dut.reset(reset);
@@ -35,79 +40,99 @@ private:
     sc_clock clock;
     sc_signal<bool> reset;
 
-    // other channels
-    sc_fifo<axi4::full::Packets::ReadAddress> arFifo;
+    unsigned numThreads;
+    std::vector<sc_fifo<axi4::full::Packets::ReadAddress>> arFifos;
+    sc_fifo<axi4::full::Packets::ReadData> rFifo;
+
+    unsigned numTransactions = 4096;
+    unsigned addrOffset = 16;
+
+    std::mt19937 mt { 566542 };
+    std::uniform_int_distribution<> distBeats { 0, 32 };
+    std::uniform_int_distribution<> distWait { 20, 50 };
 
     void entry() override {
-        auto const& axiSlaveCfg = dut.S_AXI.config();
-        auto const& axiMasterCfg = dut.S_AXI.config();
-
-        unsigned numAddress = 1024 * 16;
-        unsigned numBeat = 1;
-        unsigned idMask = (1 << axiMasterCfg.wId) - 1;
-        unsigned addrOffset = 16;
-
-        // needed for chisel modules
         resetDUTs();
+
+        for (unsigned id = 0; id < numThreads; ++id) {
+            sc_spawn([this, id] { idThread(id); });
+        }
+
+        sc_spawn([this] { m_axi_ar(); });
+        sc_spawn([this] { m_axi_r(); });
 
         sc_join j;
 
-        SC_SPAWN_TO(j) {
-            for (uint16_t i = 0; i < numAddress; ++i) {
-                axi4::full::Packets::ReadAddress ar {
-                    .id = bv_from(i & idMask),
-                    .addr = bv_from(i << addrOffset),
-                    .len = (uint8_t)(numBeat - 1)
-                };
-                dut.S_AXI.sendAR(ar);
-                fmt::print("t = {}, dut.S_AXI.sendAR({})\n", sc_time_stamp().to_string(), ar);
-            }
-        };
-
-        SC_SPAWN_TO(j) {
-            for (uint16_t i = 0; i < numAddress; ++i) {
-                for (uint16_t j = 0; j < numBeat; ++j) {
-                    auto r = dut.S_AXI.receiveR();
-                    fmt::print("t = {}, dut.S_AXI.receiveR() = {}\n", sc_time_stamp().to_string(), r);
-
-                    auto received = r.data.to_uint64();
-                    auto expected = (i << addrOffset) + j;
-
-                    fmt::print("t = {}, received = {:#010x}, expected = {:#010x}, {}\n", sc_time_stamp().to_string(), received, expected, received == expected ? "" : "*");
-
-                    ASSERT_EQ(received, expected);
-                }
-            }
-        };
-
-        SC_SPAWN_TO(j) {
-            for (uint16_t i = 0; i < numAddress; ++i) {
-                auto ar = dut.M_AXI.receiveAR();
-                fmt::print("t = {}, dut.M_AXI.receiveAR() = {}\n", sc_time_stamp().to_string(), ar);
-                arFifo.write(ar);
-            }
-        };
-
-        SC_SPAWN_TO(j) {
-            for (uint16_t i = 0; i < numAddress; ++i) {
-                auto ar = arFifo.read();
-
-                for (uint16_t j = 0; j < numBeat; ++j) {
-                    axi4::full::Packets::ReadData r {
-                        .id = ar.id,
-                        .data = bv_from(ar.addr.to_uint64() + j),
-                        .resp = 0,
-                        .last = (j == numBeat - 1)
-                    };
-                    dut.M_AXI.sendR(r);
-                    fmt::print("t = {}, dut.M_AXI.sendR({})\n", sc_time_stamp().to_string(), r);
-                }
-            }
-        };
+        j.add_process(sc_spawn([this] { s_axi_ar(); }));
+        j.add_process(sc_spawn([this] { s_axi_r(); }));
 
         j.wait();
 
         finish();
+    }
+
+    void s_axi_ar() {
+        for (uint64_t i = 0; i < numTransactions; ++i) {
+            axi4::full::Packets::ReadAddress ar {
+                .id = bv_from(i & (numThreads - 1)),
+                .addr = bv_from(i << addrOffset),
+                .len = (uint8_t) distBeats(mt)
+            };
+            dut.S_AXI.sendAR(ar);
+            fmt::print("[{:^20}] [{:^20}] dut.S_AXI.sendAR({})\n", sc_time_stamp().to_string(), "s_axi_ar", ar);
+        }
+    }
+
+    void s_axi_r() {
+        for (unsigned i = 0; i < numTransactions; ++i) {
+            for (unsigned j = 0;; ++j) {
+                auto r = dut.S_AXI.receiveR();
+                fmt::print("[{:^20}] [{:^20}] dut.S_AXI.receiveR() = {}\n", sc_time_stamp().to_string(), "s_axi_r", r);
+
+                auto received = r.data.to_uint64();
+                auto expected = (((uint64_t)i) << addrOffset) + j;
+
+                if (r.last)
+                    break;
+            }
+        }
+    }
+
+    void m_axi_ar() {
+        while (true) {
+            auto ar = dut.M_AXI.receiveAR();
+            fmt::print("[{:^20}] [{:^20}] dut.M_AXI.receiveAR() = {}\n", sc_time_stamp().to_string(), "m_axi_ar", ar);
+            arFifos.at(ar.id.to_uint64()).write(ar);
+        }
+    }
+
+    void m_axi_r() {
+        while (true) {
+            auto r = rFifo.read();
+            dut.M_AXI.sendR(r);
+            fmt::print("[{:^20}] [{:^20}] dut.M_AXI.sendR({})\n", sc_time_stamp().to_string(), "m_axi_r", r);
+        }
+    }
+
+    void idThread(uint32_t id) {
+        while (true) {
+            auto ar = arFifos[id].read();
+            fmt::print("[{:^20}] [{:^20}] arFifos[id].read() = {}\n", sc_time_stamp().to_string(), fmt::format("idThread({:^4d})", id), ar);
+
+            wait(distWait(mt), SC_NS);
+
+            for (unsigned j = 0; j <= ar.len; ++j) {
+                axi4::full::Packets::ReadData r {
+                    .id = bv_from(id),
+                    .data = bv_from(ar.addr.to_uint64() + j),
+                    .resp = 0,
+                    .last = (j == ar.len)
+                };
+                rFifo.write(r);
+
+                wait(distWait(mt), SC_NS);
+            }
+        }
     }
 
     void resetDUTs() {
