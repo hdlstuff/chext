648b7d86f21ce88cf131a0cc07ca54dad259858e
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2026-07-04 19:42:52 +0000

    Support for hierarchically-named AXI4 interfaces.

--- src/main/scala/chext/TestBench.scala
+++ src/main/scala/chext/TestBench.scala
@@ -1,6 +1,7 @@
 package chext
 
 import chisel3._
+import chisel3.experimental.SourceInfo
 
 object exportIO {
   def module(parentModule: Module, childModule: Module) = {
@@ -56,9 +57,105 @@ trait HasHdlinfoModule extends RawModule {
   def hdlinfoModule: hdlinfo.Module
 }
 
+private object Axi4HdlinfoDeclaration {
+  private val requireAxi4 =
+    chext.util.Require("TestBenchTop.declareAxi4Interface")
+
+  private sealed trait Axi4InterfaceDeclaration[-T <: Data] {
+    def cfg(interface: T): chext.amba.axi4.Config
+    def valid(interface: T)(implicit si: SourceInfo): Bool
+    def defaultKind: String
+  }
+
+  case class Result(
+      cfg: chext.amba.axi4.Config,
+      valid: Bool,
+      defaultKind: String
+  )
+
+  private implicit object Axi4HdlinfoDeclaration_Raw
+      extends Axi4InterfaceDeclaration[chext.amba.axi4.RawInterface] {
+    def cfg(interface: chext.amba.axi4.RawInterface): chext.amba.axi4.Config =
+      interface.cfg
+
+    def valid(interface: chext.amba.axi4.RawInterface)(implicit si: SourceInfo): Bool =
+      interface.ARVALID.getOrElse(
+        interface.AWVALID.getOrElse(
+          requireAxi4.failHere(
+            "cannot determine AXI4 role",
+            Seq("The interface has neither ARVALID nor AWVALID because read=false and write=false.")
+          )
+        )
+      )
+
+    val defaultKind: String = "axi4"
+  }
+
+  private implicit object Axi4HdlinfoDeclaration_Full
+      extends Axi4InterfaceDeclaration[chext.amba.axi4.full.Interface] {
+    def cfg(interface: chext.amba.axi4.full.Interface): chext.amba.axi4.Config =
+      interface.cfg
+
+    def valid(interface: chext.amba.axi4.full.Interface)(implicit si: SourceInfo): Bool =
+      if (interface.cfg.read) interface.ar.$valid
+      else if (interface.cfg.write) interface.aw.$valid
+      else
+        requireAxi4.failHere(
+          "cannot determine AXI4 full role",
+          Seq("The interface config has read=false and write=false.")
+        )
+
+    val defaultKind: String = "axi4_rtl_hier"
+  }
+
+  private implicit object Axi4HdlinfoDeclaration_Lite
+      extends Axi4InterfaceDeclaration[chext.amba.axi4.lite.Interface] {
+    def cfg(interface: chext.amba.axi4.lite.Interface): chext.amba.axi4.Config =
+      interface.cfg
+
+    def valid(interface: chext.amba.axi4.lite.Interface)(implicit si: SourceInfo): Bool =
+      if (interface.cfg.read) interface.ar.$valid
+      else if (interface.cfg.write) interface.aw.$valid
+      else
+        requireAxi4.failHere(
+          "cannot determine AXI4 lite role",
+          Seq("The interface config has read=false and write=false.")
+        )
+
+    val defaultKind: String = "axi4_rtl_hier"
+  }
+
+  def apply(interface: Data)(implicit si: SourceInfo): Result =
+    interface match {
+      case raw: chext.amba.axi4.RawInterface =>
+        applyTyped(raw)
+      case full: chext.amba.axi4.full.Interface =>
+        applyTyped(full)
+      case lite: chext.amba.axi4.lite.Interface =>
+        applyTyped(lite)
+      case other => failUnsupportedInterface(other)
+    }
+
+  private def applyTyped[T <: Data](
+      interface: T
+  )(implicit declaration: Axi4InterfaceDeclaration[T], si: SourceInfo): Result =
+    Result(declaration.cfg(interface), declaration.valid(interface), declaration.defaultKind)
+
+  private def failUnsupportedInterface(other: Data)(implicit si: SourceInfo): Nothing =
+    requireAxi4.failHere(
+      "unsupported AXI4 interface type",
+      Seq(
+        s"Actual type: ${other.getClass.getName}",
+        "Expected chext.amba.axi4.RawInterface, chext.amba.axi4.full.Interface, or chext.amba.axi4.lite.Interface."
+      )
+    )
+}
+
 /** Utility trait that allows for easier registration of the hdlinfo object.
   */
 trait TestBenchTop extends HasHdlinfoModule {
+  private val requireTestBenchTop = chext.util.Require("TestBenchTop")
+
   private val encodedDataBuilder = new chext.util.EncodedDataBuilder()
   private val ports = scala.collection.mutable.ArrayBuffer.empty[hdlinfo.Port]
   private val interfaces = scala.collection.mutable.ArrayBuffer.empty[hdlinfo.Interface]
@@ -80,7 +177,7 @@ trait TestBenchTop extends HasHdlinfoModule {
     )
   }
 
-  private def getValidDirection(x: Data): hdlinfo.PortDirection = {
+  private def getValidDirection(x: Data)(implicit si: SourceInfo): hdlinfo.PortDirection = {
     import chisel3.reflect.DataMirror
 
     @scala.annotation.nowarn
@@ -92,7 +189,11 @@ trait TestBenchTop extends HasHdlinfoModule {
       case ActualDirection.Unspecified        => hdlinfo.PortDirection.none
       case ActualDirection.Bidirectional(dir) => hdlinfo.PortDirection.none
     }
-    require(result.str != "none")
+    requireTestBenchTop.here(
+      result.str != "none",
+      "port direction must be input or output",
+      Seq(s"Chisel direction: ${DataMirror.directionOf(x)}")
+    )
 
     result
   }
@@ -102,8 +203,12 @@ trait TestBenchTop extends HasHdlinfoModule {
   final def declareArg[T: io.circe.Encoder](
       name: String,
       typedObject: hdlinfo.TypedObject
-  ): Unit = {
-    require(!hasArgNamed(name))
+  )(implicit si: SourceInfo): Unit = {
+    requireTestBenchTop.here(
+      !hasArgNamed(name),
+      "duplicate hdlinfo argument",
+      Seq(s"Argument name: $name")
+    )
     args.addOne(name -> typedObject)
     argNames.addOne(name)
   }
@@ -114,11 +219,23 @@ trait TestBenchTop extends HasHdlinfoModule {
       frequencyMHz: Float = 100,
       associatedReset: String = "reset",
       portName: Option[String] = None
-  ): Unit = {
+  )(implicit si: SourceInfo): Unit = {
     val name = nameFor(clock, portName)
-    require(!hasPortNamed(name))
+    requireTestBenchTop.here(
+      !hasPortNamed(name),
+      "duplicate hdlinfo port",
+      Seq(s"Port name: $name")
+    )
 
-    require(sensitivity.str.startsWith("clock"))
+    requireTestBenchTop.here(
+      sensitivity.str.startsWith("clock"),
+      "invalid clock sensitivity",
+      Seq(
+        s"Port name: $name",
+        s"Sensitivity: ${sensitivity.str}",
+        "Expected a sensitivity whose name starts with 'clock'."
+      )
+    )
 
     ports.addOne(
       hdlinfo.Port(
@@ -141,11 +258,23 @@ trait TestBenchTop extends HasHdlinfoModule {
       sensitivity: hdlinfo.PortSensitivity = hdlinfo.PortSensitivity.resetActiveHigh,
       associatedClock: String = "clock",
       portName: Option[String] = None
-  ): Unit = {
+  )(implicit si: SourceInfo): Unit = {
     val name = nameFor(reset, portName)
-    require(!hasPortNamed(name))
+    requireTestBenchTop.here(
+      !hasPortNamed(name),
+      "duplicate hdlinfo port",
+      Seq(s"Port name: $name")
+    )
 
-    require(sensitivity.str.startsWith("reset"))
+    requireTestBenchTop.here(
+      sensitivity.str.startsWith("reset"),
+      "invalid reset sensitivity",
+      Seq(
+        s"Port name: $name",
+        s"Sensitivity: ${sensitivity.str}",
+        "Expected a sensitivity whose name starts with 'reset'."
+      )
+    )
 
     ports.addOne(
       hdlinfo.Port(
@@ -179,11 +308,23 @@ trait TestBenchTop extends HasHdlinfoModule {
       associatedClock: String = "clock",
       associatedReset: String = "reset",
       portName: Option[String] = None
-  ): Unit = {
+  )(implicit si: SourceInfo): Unit = {
     val name = nameFor(interrupt, portName)
-    require(!hasPortNamed(name))
+    requireTestBenchTop.here(
+      !hasPortNamed(name),
+      "duplicate hdlinfo port",
+      Seq(s"Port name: $name")
+    )
 
-    require(sensitivity.str.startsWith("interrupt"))
+    requireTestBenchTop.here(
+      sensitivity.str.startsWith("interrupt"),
+      "invalid interrupt sensitivity",
+      Seq(
+        s"Port name: $name",
+        s"Sensitivity: ${sensitivity.str}",
+        "Expected a sensitivity whose name starts with 'interrupt'."
+      )
+    )
 
     ports.addOne(
       hdlinfo.Port(
@@ -202,34 +343,60 @@ trait TestBenchTop extends HasHdlinfoModule {
   }
 
   final def declareAxi4Interface(
-      interface: chext.amba.axi4.RawInterface,
+      interface: Data,
       associatedClock: String = "clock",
       associatedReset: String = "reset",
       interfaceName: Option[String] = None,
-      kind: String = "axi4",
+      kind: String = "",
       args: Map[String, hdlinfo.TypedObject] = Map.empty
-  ): Unit = {
-    import io.circe.generic.auto._
-
+  )(implicit si: SourceInfo): Unit = {
     val name = nameFor(interface, interfaceName)
-    require(!hasInterfaceNamed(name))
+    requireTestBenchTop.here(
+      !hasInterfaceNamed(name),
+      "duplicate hdlinfo interface",
+      Seq(s"Interface name: $name")
+    )
 
-    val role = {
-      import chisel3.reflect.DataMirror
+    val declaration = Axi4HdlinfoDeclaration(interface)
 
-      DataMirror.directionOf(
-        interface.ARVALID.getOrElse(
-          interface.AWVALID.getOrElse(
-            throw new RuntimeException("Cannot determine the role of the AXI4 interface!")
+    declareAxi4InterfaceImpl(
+      name,
+      axi4RoleFromValid(declaration.valid),
+      declaration.cfg,
+      associatedClock,
+      associatedReset,
+      if (kind.isEmpty) declaration.defaultKind else kind,
+      args
+    )
+  }
+
+  private def axi4RoleFromValid(valid: Bool)(implicit si: SourceInfo): hdlinfo.InterfaceRole = {
+    import chisel3.reflect.DataMirror
+
+    DataMirror.directionOf(valid) match {
+      case ActualDirection.Input  => hdlinfo.InterfaceRole.slave
+      case ActualDirection.Output => hdlinfo.InterfaceRole.master
+      case _ =>
+        requireTestBenchTop.failHere(
+          "cannot determine AXI4 interface role",
+          Seq(
+            s"Valid signal direction: ${DataMirror.directionOf(valid)}",
+            "Expected an input valid signal for a slave role or an output valid signal for a master role."
           )
         )
-      ) match {
-        case ActualDirection.Input  => hdlinfo.InterfaceRole.slave
-        case ActualDirection.Output => hdlinfo.InterfaceRole.master
-        case _ =>
-          throw new RuntimeException("Cannot determine the role of the AXI4 interface!")
-      }
     }
+  }
+
+  private def declareAxi4InterfaceImpl(
+      name: String,
+      role: hdlinfo.InterfaceRole,
+      cfg: chext.amba.axi4.Config,
+      associatedClock: String,
+      associatedReset: String,
+      kind: String,
+      args: Map[String, hdlinfo.TypedObject]
+  ): Unit = {
+    import io.circe.generic.auto._
 
     interfaces.addOne(
       hdlinfo.Interface(
@@ -238,7 +405,7 @@ trait TestBenchTop extends HasHdlinfoModule {
         hdlinfo.InterfaceKind(kind),
         associatedClock,
         associatedReset,
-        Map("config" -> hdlinfo.TypedObject(interface.cfg)) ++ args
+        Map("config" -> hdlinfo.TypedObject(cfg)) ++ args
       )
     )
     interfaceNames.addOne(name)
@@ -251,11 +418,15 @@ trait TestBenchTop extends HasHdlinfoModule {
       interfaceName: Option[String] = None,
       kind: String = "axi4s",
       args: Map[String, hdlinfo.TypedObject] = Map.empty
-  ): Unit = {
+  )(implicit si: SourceInfo): Unit = {
     import io.circe.generic.auto._
 
     val name = nameFor(interface, interfaceName)
-    require(!hasInterfaceNamed(name))
+    requireTestBenchTop.here(
+      !hasInterfaceNamed(name),
+      "duplicate hdlinfo interface",
+      Seq(s"Interface name: $name")
+    )
 
     val role = {
       import chisel3.reflect.DataMirror
@@ -264,7 +435,13 @@ trait TestBenchTop extends HasHdlinfoModule {
         case ActualDirection.Input  => hdlinfo.InterfaceRole.slave
         case ActualDirection.Output => hdlinfo.InterfaceRole.master
         case _ =>
-          throw new RuntimeException("Cannot determine the role of the AXI4-stream interface!")
+          requireTestBenchTop.failHere(
+            "cannot determine AXI4-stream interface role",
+            Seq(
+              s"Interface name: $name",
+              s"TDATA direction: ${DataMirror.directionOf(interface.TDATA)}"
+            )
+          )
       }
     }
 
@@ -289,9 +466,13 @@ trait TestBenchTop extends HasHdlinfoModule {
       interfaceName: Option[String] = None,
       kind: String = "readyValid[$]",
       args: Map[String, hdlinfo.TypedObject] = Map.empty
-  ): Unit = {
+  )(implicit si: SourceInfo): Unit = {
     val name = nameFor(interface, interfaceName)
-    require(!hasInterfaceNamed(name))
+    requireTestBenchTop.here(
+      !hasInterfaceNamed(name),
+      "duplicate hdlinfo interface",
+      Seq(s"Interface name: $name")
+    )
 
     val role = {
       import chisel3.reflect.DataMirror
@@ -300,7 +481,13 @@ trait TestBenchTop extends HasHdlinfoModule {
         case ActualDirection.Input  => hdlinfo.InterfaceRole("source")
         case ActualDirection.Output => hdlinfo.InterfaceRole("sink")
         case _ =>
-          throw new RuntimeException("Cannot determine the role of the AXI4-stream interface!")
+          requireTestBenchTop.failHere(
+            "cannot determine elastic interface role",
+            Seq(
+              s"Interface name: $name",
+              s"bits direction: ${DataMirror.directionOf(interface.$bits)}"
+            )
+          )
       }
     }
 
@@ -330,16 +517,50 @@ trait TestBenchTop extends HasHdlinfoModule {
 
     portsSeq.foreach { case port =>
       if (port.kind.str == "clock") {
-        require(hasPortNamed(port.associatedReset))
+        requireTestBenchTop(
+          hasPortNamed(port.associatedReset),
+          "clock port references an unknown reset",
+          Seq(
+            s"Clock port: ${port.name}",
+            s"Associated reset: ${port.associatedReset}",
+            s"Known ports: ${portNames.toSeq.sorted.mkString(", ")}"
+          )
+        )
       } else if (port.kind.str == "reset") {
-        require(hasPortNamed(port.associatedClock))
+        requireTestBenchTop(
+          hasPortNamed(port.associatedClock),
+          "reset port references an unknown clock",
+          Seq(
+            s"Reset port: ${port.name}",
+            s"Associated clock: ${port.associatedClock}",
+            s"Known ports: ${portNames.toSeq.sorted.mkString(", ")}"
+          )
+        )
       } else {
-        require(hasPortNamed(port.associatedClock) && hasPortNamed(port.associatedReset))
+        requireTestBenchTop(
+          hasPortNamed(port.associatedClock) && hasPortNamed(port.associatedReset),
+          "port references an unknown clock or reset",
+          Seq(
+            s"Port: ${port.name}",
+            s"Associated clock: ${port.associatedClock}",
+            s"Associated reset: ${port.associatedReset}",
+            s"Known ports: ${portNames.toSeq.sorted.mkString(", ")}"
+          )
+        )
       }
     }
 
     interfacesSeq.foreach { case interface =>
-      require(hasPortNamed(interface.associatedClock) && hasPortNamed(interface.associatedReset))
+      requireTestBenchTop(
+        hasPortNamed(interface.associatedClock) && hasPortNamed(interface.associatedReset),
+        "interface references an unknown clock or reset",
+        Seq(
+          s"Interface: ${interface.name}",
+          s"Associated clock: ${interface.associatedClock}",
+          s"Associated reset: ${interface.associatedReset}",
+          s"Known ports: ${portNames.toSeq.sorted.mkString(", ")}"
+        )
+      )
     }
 
     hdlinfo.Module(
